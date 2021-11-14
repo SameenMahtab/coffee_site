5e52364c03cbf38f2f2ae663fb59a20eaa6770f8
SameenMahtab <sameen.mahtab.e@gmail.com>
2021-11-13 19:51:54 -0500

    need to debud

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,7 +1,26 @@
 import { CoffeesController } from './coffees.controller';
 import { Module } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
+import { EventSchema } from 'src/events/entities/event.entity';
 
-@Module({controllers: [ CoffeesController], providers: [CoffeesService], exports: [CoffeesService]})
+@Module({ 
+    imports: [
+    MongooseModule.forFeature([
+        { 
+            name: Coffee.name,
+            schema: CoffeeSchema,
+        },
+        {
+            name: Event.name,
+            schema: EventSchema,
+        },
+    ]),
+    ],
+    controllers: [CoffeesController],
+    providers: [CoffeesService],
+})
+
 export class CoffeesModule {}
  
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,22 @@
-export class Coffee {
-    id: number;
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Coffee extends Document {
+    // id: any;
+    // save() {
+    //   throw new Error('Method not implemented.');
+    // }
+    @Prop()
     name: string;
+
+    @Prop()
     brand: string;
+
+    @Prop({ default: 0 })
+    recommendations: number;
+
+    @Prop([String])
     flavours: string[];
 }
+
+export const CoffeeSchema = SchemaFactory.createForClass(Coffee);
